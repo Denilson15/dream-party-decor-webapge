68af2c80e4c4febcb864518c646ba4601a1c57c8
Denilson15 <galvarado1501@gmail.com>
2025-04-18 13:34:40 -0700

    Initialized web page with design draft complete

---
+++ index.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const date = document.getElementById("date");
+const submitBtn = document.getElementById("submit-btn");
